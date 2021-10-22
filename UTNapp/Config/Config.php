967854e456d8a87272ab58f05cd8d868fc714798
moretvo <84353229+moretvo@users.noreply.github.com>
2021-10-21 23:59:50 -0300

    Push Config API

--- UTNapp/Config/Config.php
+++ UTNapp/Config/Config.php
@@ -1,11 +1,15 @@
 <?php namespace Config;
 
-define("ROOT", dirname(__DIR__) . "/");
+define("ROOT", dirname(__DIR__) . "/"); 
 //Path to your project's root folder
 define("FRONT_ROOT", "/UTNapp/UTNapp/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
 define("IMG_PATH", VIEWS_PATH . "img/");
+
+//Path to API
+define("URLAPI", "https://utn-students-api.herokuapp.com/api/Student");
+define("HTTPHEADER", ['x-api-key:4f3bceed-50ba-4461-a910-518598664c08']);
 ?>
 
