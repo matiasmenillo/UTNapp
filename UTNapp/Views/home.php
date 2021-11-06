320dd433233114e10c062680d825f84b306d1e91
mauroporzio <mauroporzio02@gmail.com>
2021-11-06 12:05:36 -0300

    Fix Shown data of admin in home.php

--- UTNapp/Views/home.php
+++ UTNapp/Views/home.php
@@ -4,7 +4,7 @@
     if($_SESSION["loggedUser"]->getAdmin() == 1){
       include_once('nav-barAdmin.php'); 
     }
-    if($_SESSION["loggedUser"]->getAdmin() == 0)
+    else
     {
       include_once('nav-barStudent.php');
     }
@@ -21,8 +21,8 @@
     <table style="text-align:center; color:orange">
     <thead>
         <tr>
-            <th style="width: 3%;">Legajo</th>
-            <th style="width: 1%;">Carrera</th>
+            <?php if ($_SESSION["loggedUser"]->getAdmin() == 0) {?><th style="width: 3%;">Legajo</th><?php }?>
+            <?php if ($_SESSION["loggedUser"]->getAdmin() == 0) {?><th style="width: 1%;">Carrera</th><?php }?>
             <th style="width: 1%;">Sexo</th>
             <th style="width: 1%;">Fecha de Nacimiento</th>
             <th style="width: 1%;">Email</th>
@@ -31,12 +31,13 @@
   </thead>
   <tbody>
   <tr>
-      <td style="color:black"><?php echo $_SESSION["loggedUser"]->getFileNumber(); ?></td>
-      <td style="color:black"><?php echo $_SESSION["loggedUserCareer"]->getDescription(); ?></td>
+      <?php if ($_SESSION["loggedUser"]->getAdmin() == 0) {?><td style="color:black"><?php echo $_SESSION["loggedUser"]->getFileNumber(); ?></td><?php }?>
+      <?php if ($_SESSION["loggedUser"]->getAdmin() == 0) {?><td style="color:black"><?php echo $_SESSION["loggedUserCareer"]->getDescription(); ?></td><?php }?>
       <td style="color:black"><?php echo $_SESSION["loggedUser"]->getGender(); ?></td>
       <td style="color:black"><?php echo $_SESSION["loggedUser"]->getBirthDate(); ?></td>
       <td style="color:black"><?php echo $_SESSION["loggedUser"]->getEmail(); ?></td>
       <td style="color:black"><?php echo $_SESSION["loggedUser"]->getPhoneNumber(); ?></td>
+  </tr>
   </tbody>
     </table>
 
