ceff76c501fbc1816538474944d022deede4401f
moretvo <84353229+moretvo@users.noreply.github.com>
2021-11-09 19:24:34 -0300

    Add files via upload

--- UTNAppDB/Scripts UTNAppDB/GetDBReady/(2) ADD_ADMINS.sql
+++ UTNAppDB/Scripts UTNAppDB/GetDBReady/(2) ADD_ADMINS.sql
@@ -2,3 +2,4 @@
 
 CALL InsertStudent((SELECT MAX(IdStudent) FROM Student) + 1, 'Mauro', 'Porzio', 'mauro@gmail.com', 1234, 1231231, 1, 1, null, 'M', '2021-12-02', 22323454, 1);
 CALL InsertStudent((SELECT MAX(IdStudent) FROM Student) + 1, 'Matias', 'Menillo', 'matias@gmail.com', 1234, 1112231, 1, 2, null, 'M', '2000-01-01', 22354363, 1);
+CALL InsertStudent((SELECT MAX(IdStudent) FROM Student) + 1, 'Rodrigo', 'Moreno', 'rodrigo@gmail.com', 1234, 1234123, 1, 2, null, 'M', '2000-01-01', 22354662, 1);
