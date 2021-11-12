a1f5e7ff662876ac2b22b2644f862c4482a3e8e3
mauroporzio <mauroporzio02@gmail.com>
2021-11-12 17:34:00 -0300

    fix add admin

--- UTNAppDB/Scripts UTNAppDB/GetDBReady/(2) ADD_ADMINS.sql
+++ UTNAppDB/Scripts UTNAppDB/GetDBReady/(2) ADD_ADMINS.sql
@@ -1,3 +1,2 @@
 /* RUN TO ADD ADMINS (CORRER LUEGO DE LEVANTAR LA API DESDE LOGIN EN APP) */
-
-CALL InsertUser('Admin', 'Admin', 'admin@admin.com', 1234, 00000000, 1);
+CALL InsertUser('Admin', 'Admin', 'admin@admin.com', 1234, 1);
